194f4197303d82bb9d6ea998050f4834b29125f5
Alex Shirazi <alexshirazi@Alexs-MacBook-Pro.local>
2023-12-13 13:25:53 -0800

    setup

---
+++ my-personal-site/src/components/Experience.js
@@ -0,0 +1,28 @@
+const experiences = [
+    {
+        company: 'Santa Clara University',
+        location: 'Santa Clara, CA',
+        duration: 'September 2023 – March 2025',
+        role: 'Computer Science B.S',
+        logo: `${process.env.PUBLIC_URL}/SCU-Logo.png`,
+        tasks: [
+            'Actively pursuing a specialization in Artificial Intelligence, with a keen interest in exploring its applications and innovations',
+            'Committed to deepening my understanding and contributing to advancements in the AI domain',
+        ]
+    },
+
+    {
+        company: 'Foothill College',
+        location: 'Los Altos Hills, CA',
+        duration: 'June 2021 – June 2023',
+        role: 'Degree For Transfer',
+        logo: `${process.env.PUBLIC_URL}/Foothill-Logo.png`,
+        tasks: [
+            'Actively engaged in mastering C++ at Foothill College, complemented by additional coursework in Python and Java',
+            'Achieved an outstanding GPA of 3.92, establishing a robust foundation for advanced studies in the field of computer science',
+        ]
+    }
+
+];
+
+export default experiences;
